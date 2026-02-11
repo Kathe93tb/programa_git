19fa3dfe53ab6fc2cb83d015791c492949d6b4dd
Kathe93tb <Kathe9376@gmail.com>
2026-02-10 20:40:01 -0500

    Delete formulario directory

--- formulario/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conex = mysqli_connect("localhost", "root", "", "formulario", 3306);
-
-if (!$conex) {
-    die("No se pudo conectar: " . mysqli_connect_error());
-}
-
-echo "Conexion exitosa";
-?>
